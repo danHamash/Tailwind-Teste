eae0b2ebb0d278dbe3e8d757f67f8f2b04a60923
danHamash <megadestroier@hotmail.com>
2025-12-28 19:14:49 -0300

    update engine

---
+++ engine.js
@@ -0,0 +1,22 @@
+
+  const btn = document.querySelector("button");
+  const menu = document.getElementById("mobileMenu");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    menu.classList.add("flex");
+    menu.classList.add("flex-col");
+    menu.classList.add("items-center");
+    menu.classList.add("z-50");
+    menu.classList.add("bg-nav")
+    menu.classList.add("absolute");
+    menu.classList.add("top-0");
+    menu.classList.add("right-0");
+    menu.classList.add("h-[50vh]");
+    menu.classList.add("w-full");
+    menu.classList.add("gap-4");
+    menu.classList.add("duration-200");
+    menu.classList.add("text-center");
+    menu.classList.add("pt-8");
+    
+  });
