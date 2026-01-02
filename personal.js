23ea82a16c507474b91ab05bd9548135a1b422ce
danHamash <megadestroier@hotmail.com>
2026-01-02 17:49:02 -0300

    added contacto area

---
+++ personal.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              muted: "#64748b",
+
+              primary: "#4f46e5",
+              primaryHover: "#4338ca",
+              danger: "#ef4444",
+              dangerHover: "#be0a0a",
+              subtitle: "#28273f",
+              linkNav: "#f8e7a1",
+              linkNavHover: "#d7b466",
+              bodyBg: "#f5f5f5",
+              border: "#e5e7eb",
+              nav: "#181737",
+              headerPlusBg: "#cfcfcf1b",
+            },
+            backgroundImage: {
+              hero: "url('https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=1600')",
+              card1: "url('https://unsplash.com/pt-br/fotografias/uma-casa-com-uma-porta-de-entrada-azul-e-uma-porta-de-entrada-marrom-xaqsFfoEq3o')",
+            },
+          },
+        },
+      };
